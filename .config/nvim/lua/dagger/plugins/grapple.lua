ead10c118d20a3baca76e476f079eba724752d7b
JuanBaut <deserd@protonmail.com>
2025-01-10 23:43:08 -0500

    fixed grapple typo

--- .config/nvim/lua/dagger/plugins/grapple.lua
+++ .config/nvim/lua/dagger/plugins/grapple.lua
@@ -20,7 +20,7 @@ return {
     vim.keymap.set("n", "m1", "<cmd>Grapple select index=1<cr>", { desc = "Grapple 1" })
     vim.keymap.set("n", "m2", "<cmd>Grapple select index=2<cr>", { desc = "Grapple 2" })
     vim.keymap.set("n", "m3", "<cmd>Grapple select index=3<cr>", { desc = "Grapple 3" })
-    vim.keymap.set("n", "m3", "<cmd>Grapple select index=3<cr>", { desc = "Grapple 3" })
+    vim.keymap.set("n", "m4", "<cmd>Grapple select index=3<cr>", { desc = "Grapple 4" })
     vim.keymap.set("n", "m5", "<cmd>Grapple select index=5<cr>", { desc = "Grapple 5" })
     vim.keymap.set("n", "m6", "<cmd>Grapple select index=6<cr>", { desc = "Grapple 6" })
 
