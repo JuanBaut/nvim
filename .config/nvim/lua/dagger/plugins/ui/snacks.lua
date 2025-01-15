07c1ffa1b0f3df56f18c9148533568f75bac7aa8
JuanBaut <deserd@protonmail.com>
2025-01-14 21:38:05 -0500

    added snacks for the scope indent
    deleted older indent plugins, not the functionality I was looking for

---
+++ .config/nvim/lua/dagger/plugins/ui/snacks.lua
@@ -0,0 +1,18 @@
+return {
+  "folke/snacks.nvim",
+  opts = {
+    indent = {
+      indent = {
+        enabled = false,
+      },
+      scope = {
+        only_current = true,
+        underline = true,
+        char = "▏",
+      },
+      animate = {
+        enabled = false,
+      },
+    },
+  },
+}
