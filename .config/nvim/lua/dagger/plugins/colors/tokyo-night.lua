5ae78ac375f1eb56ad8675ca3527650ef4c4775f
JuanBaut <deserd@protonmail.com>
2025-01-16 12:22:25 -0500

    reverted to dark theme

--- .config/nvim/lua/dagger/plugins/colors/tokyo-night.lua
+++ .config/nvim/lua/dagger/plugins/colors/tokyo-night.lua
@@ -7,16 +7,16 @@ return {
     require("tokyonight").setup({
       transparent = false,
       --dim_inactive = true,
-      --on_colors = function(colors)
-      --  colors.bg = "#0D1017"
-      --  colors.bg_dark = "#0D1017"
-      --  colors.bg_float = "#131621"
-      --  colors.bg_popup = "#131621"
-      --  colors.bg_search = "#131621"
-      --  colors.bg_sidebar = "#131621"
-      --  colors.bg_statusline = "#131621"
-      --end,
+      on_colors = function(colors)
+        colors.bg = "#0D1017"
+        colors.bg_dark = "#0D1017"
+        colors.bg_float = "#131621"
+        colors.bg_popup = "#131621"
+        colors.bg_search = "#131621"
+        colors.bg_sidebar = "#131621"
+        colors.bg_statusline = "#131621"
+      end,
     })
-    vim.cmd.colorscheme("tokyonight-day")
+    vim.cmd.colorscheme("tokyonight-night")
   end,
 }
