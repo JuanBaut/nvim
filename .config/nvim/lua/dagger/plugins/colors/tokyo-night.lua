df2f665b47aea6231fd5087d7e27ead8d616a37a
JuanBaut <deserd@protonmail.com>
2025-01-16 12:13:39 -0500

    switched to light theme

--- .config/nvim/lua/dagger/plugins/colors/tokyo-night.lua
+++ .config/nvim/lua/dagger/plugins/colors/tokyo-night.lua
@@ -6,17 +6,17 @@ return {
   config = function()
     require("tokyonight").setup({
       transparent = false,
-      dim_inactive = true,
-      on_colors = function(colors)
-        colors.bg = "#0D1017"
-        colors.bg_dark = "#0D1017"
-        colors.bg_float = "#131621"
-        colors.bg_popup = "#131621"
-        colors.bg_search = "#131621"
-        colors.bg_sidebar = "#131621"
-        colors.bg_statusline = "#131621"
-      end,
+      --dim_inactive = true,
+      --on_colors = function(colors)
+      --  colors.bg = "#0D1017"
+      --  colors.bg_dark = "#0D1017"
+      --  colors.bg_float = "#131621"
+      --  colors.bg_popup = "#131621"
+      --  colors.bg_search = "#131621"
+      --  colors.bg_sidebar = "#131621"
+      --  colors.bg_statusline = "#131621"
+      --end,
     })
-    vim.cmd.colorscheme("tokyonight-moon")
+    vim.cmd.colorscheme("tokyonight-day")
   end,
 }
