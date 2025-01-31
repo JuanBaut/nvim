922b5246373e8c90332de05292ed38560384769b
JuanBaut <deserd@protonmail.com>
2025-01-30 20:43:17 -0500

    neovide config opts

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -2,5 +2,8 @@ require("dagger.core")
 require("dagger.lazy")
 
 if vim.g.neovide then
-	vim.o.guifont = "GeistMono Nerd Font Propo:h17"
+  vim.o.guifont = "Input Mono,Symbols Nerd Font Mono:h12"
+  vim.opt.linespace = 3
+  vim.g.neovide_show_border = true
+  vim.g.neovide_scroll_animation_lenght = 0.1
 end
