92615a1a6714e17cc7624907093bb6cb7a0efafb
JuanBaut <deserd@protonmail.com>
2025-01-31 13:43:34 -0500

    switched to luasnip

--- .config/nvim/lua/dagger/plugins/lsp/blink-cmp.lua
+++ .config/nvim/lua/dagger/plugins/lsp/blink-cmp.lua
@@ -1,8 +1,32 @@
 return {
   "saghen/blink.cmp",
-  dependencies = "rafamadriz/friendly-snippets",
+  --dependencies = "rafamadriz/friendly-snippets",
+
+  dependencies = {
+    "L3MON4D3/LuaSnip",
+    lazy = true,
+    dependencies = {
+      {
+        "rafamadriz/friendly-snippets",
+        config = function()
+          require("luasnip.loaders.from_vscode").lazy_load()
+          require("luasnip.loaders.from_vscode").lazy_load({
+            paths = { vim.fn.stdpath("config") .. "/snippets" },
+          })
+        end,
+      },
+    },
+    opts = {
+      history = true,
+      delete_check_events = "TextChanged",
+    },
+  },
   version = "*",
   opts = {
+    snippets = {
+      preset = "luasnip",
+    },
+
     keymap = {
       ["<C-l>"] = { "snippet_forward", "fallback" },
       ["<C-h>"] = { "snippet_backward", "fallback" },
