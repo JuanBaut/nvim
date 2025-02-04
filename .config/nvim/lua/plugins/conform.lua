8be5c9dc2d57effec59dc00743ddd1ccea9053bd
JuanBaut <deserd@protonmail.com>
2025-02-03 23:58:35 -0500

    fixed biome with conform

--- .config/nvim/lua/plugins/conform.lua
+++ .config/nvim/lua/plugins/conform.lua
@@ -4,6 +4,10 @@ return {
   config = function()
     local conform = require("conform")
 
+    conform.formatters.biome = {
+      append_args = { "--indent-style=space" },
+    }
+
     conform.setup({
       formatters_by_ft = {
         javascript = { "biome", "rustywind" },
@@ -37,30 +41,5 @@ return {
         timeout_ms = 2000,
       })
     end, { desc = "Make Pretty" })
-
-    -- vim.keymap.set("v", "<leader>mt", "
-
-    -- vim.keymap.set("n", "<leader>mr", function()
-    --   vim.notify("Restarting prettierd...")
-    --   vim.fn.system("prettierd restart")
-    -- end, { desc = "Make Restart" })
-
-    -- vim.api.nvim_create_autocmd({ "BufWritePost" }, {
-    --   group = vim.api.nvim_create_augroup("RestartPrettierd", { clear = true }),
-    --   pattern = "*prettier*",
-    --   callback = function()
-    --     vim.notify("Restarting prettierd...")
-    --     vim.fn.system("prettierd restart")
-    --   end,
-    -- })
-
-    -- vim.api.nvim_create_autocmd("VimLeavePre", {
-    --   group = vim.api.nvim_create_augroup("StopPrettierd", { clear = true }),
-    --   pattern = { "*.ts", "*.tsx", "*.js", "*.jsx", "*.css", "*.html", "*.json" },
-    --   callback = function()
-    --     vim.notify("Stopping prettierd...")
-    --     vim.fn.system("prettierd stop")
-    --   end,
-    -- })
   end,
 }
